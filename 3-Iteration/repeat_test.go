0c43bb4b18fbed3cac8d1887ac95b3616f247029
Alejandro Henestroza <alehenestroza@gmail.com>
2022-10-09 00:13:02 -0300

    Added Repeat example

--- 3-Iteration/repeat_test.go
+++ 3-Iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 7)
@@ -15,4 +18,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeatedChar := Repeat("w", 6)
+	fmt.Println(repeatedChar)
+	// Output: wwwwww
 }
