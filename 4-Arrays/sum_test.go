49da61aaba284a755eb05986105a8351e8e5a7eb
Alejandro Henestroza <alehenestroza@gmail.com>
2022-10-09 00:42:34 -0300

    Refactored tests

--- 4-Arrays/sum_test.go
+++ 4-Arrays/sum_test.go
@@ -17,21 +17,24 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
+	checkSum := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+
 	t.Run("sum the tails of slices", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 	
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Expected %d but got %d ", want, got)
-		}
+		checkSum(t, got, want)
 	})
 
 	t.Run("sum an empty slice", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 
-		if!reflect.DeepEqual(got, want) {
-			t.Errorf("Expected %d but got %d ", want, got)
-		}
+		checkSum(t, got, want)
 	})
 }
