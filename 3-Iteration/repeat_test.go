cc6b18c5f53a3401a11e16afab267cfae0630a86
Alejandro Henestroza <alehenestroza@gmail.com>
2022-10-09 00:11:55 -0300

    Repeat now runs X times, added Benchmark

--- 3-Iteration/repeat_test.go
+++ 3-Iteration/repeat_test.go
@@ -3,11 +3,16 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 7)
+	expected := "aaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("Expected %s but got %s", expected, repeated)
 	}
 }
 
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
