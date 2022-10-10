ee05974d5e9a856f4c2fa6d4034dd1b28cf94c92
Alejandro Henestroza <alehenestroza@gmail.com>
2022-10-09 22:42:55 -0300

    Sync and mutex

---
+++ 13-Sync/sync_test.go
@@ -0,0 +1,42 @@
+package sync
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := NewCounter()
+		counter.Increment()
+		counter.Increment()
+		counter.Increment()
+
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("counter runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Increment()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, counter *Counter, want int) {
+	t.Helper()
+	if counter.Value() != want {
+		t.Errorf("got %d, want %d", counter.Value(), want)
+	}
+}
